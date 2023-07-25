a9f9e3d305727e733aacb5f5a3d4a51959374252
zhaolei <im.zhaolei@foxmail.com>
2023-07-25 21:32:24 +0800

    add hash big file demo

---
+++ golang/hash-big-file/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"crypto/sha256"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
+
+var filepath string
+
+func init() {
+	flag.StringVar(&filepath, "filepath", "", "指定文件路径")
+}
+
+func main() {
+	flag.Parse()
+	if filepath == "" {
+		flag.Usage()
+		return
+	}
+
+	file, err := os.Open(filepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	h := sha256.New()
+	buf := make([]byte, 20)
+	for {
+		n, err := file.Read(buf)
+		if err != nil {
+			break
+		}
+		_, err = h.Write(buf[:n])
+		if err != nil {
+			log.Printf("write buf to hash err: %s", err)
+		}
+	}
+
+	bs := h.Sum(nil)
+	fmt.Printf("%x\n", bs)
+}
